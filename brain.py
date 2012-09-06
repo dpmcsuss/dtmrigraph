b6332ac8bc9d67b5393205d2277df948a6be0c76
Daniel Sussman <dsussma3@jhu.edu>
2012-09-06 11:02:07 -0400

    updated for BLSA etc

--- brain.py
+++ brain.py
@@ -3,50 +3,69 @@ Created on Mar 12, 2012
 
 @author: dsussman
 '''
-import pyximport;
-pyximport.install()
 
 import os
 import numpy as np
-from scipy.io import matlab
 from scipy import sparse as sp 
+from scipy.io import loadmat, savemat
+
+from matplotlib import pyplot as plt
+from mayavi import mlab
+from collections import Counter
+from mayavi import mlab
+    
 import roi
 import fibergraph
 import zindex
 import Embed
-from matplotlib import pyplot as plt
-from mayavi import mlab
-from scipy.io import loadmat, savemat
-from collections import Counter
-from mayavi import mlab
+import lcc
+import adjacency
+
+from stfpSim import fastaprnn as FNN
 
 
-def load_data(fn):
-    m=matlab.loadmat(fn)
-    g_all=m['fibergraph']
-    n_all = np.shape(g_all)[0]
-    non_iso = g_all.dot(np.ones(n_all)).nonzero()[0]
-    g = g_all[non_iso[:,np.newaxis], non_iso]
+graphDir = '/mnt/braingraph1data/projects/MRN/graphs/biggraphs/'
+roiDir = '/mnt/braingraph1data/projects/MRN/base/roi/'
+ccDir = '/data/biggraphs/connectedcomp/'
+figDir = '/home/dsussman/Dropbox/Figures/DTMRI/lccPics/'
+faDir = '/mnt/braingraph1data/projects/MRN/base/fa/'
+mprDir = '/mnt/braingraph1data/projects/MRN/base/input/mprage_ss_crop/'
+embDir = '/data/biggraphs/embedding/'
+brainFiles = np.sort([fn.split('_')[0] for fn in os.listdir(ccDir)])
+bfn = brainFiles[0]
+
+mrnDict =  {"brainfiles":brainFiles,
+            "roiDir":roiDir,
+            "graphDir":graphDir,
+            "embedDir":embDir,
+            "lccDir":ccDir,
+}
+
+
+
+blsaDir = '/mnt/braingraph1data/projects/BLSA/aug12-mrcap/'
+blsaBigGraph = blsaDir+'graphs/big/'
+blsaROI = blsaDir+'intermediate/roi/'
+blsaFiles = np.sort([fn.split('_')[0] for fn in os.listdir(blsaBigGraph)])
+blsaLCC = '/data/projects/BLSA/graphs/lcc/'
+blsaEmb = '/data/projects/BLSA/graphs/embed/'
+
+blsaDict = {"brainfiles":blsaFiles,
+            "roiDir":blsaROI,
+            "graphDir":blsaBigGraph,
+            "embedDir":blsaEmb,
+            "lccDir":blsaLCC,
+}
     
-    ncc,cc_label = sp.cs_graph_components(g)
+def load_fibergraph(roiDir, graphDir, bfn):
     
-    cc_size = [(cc,np.sum(cc_label==cc)) for cc in set(cc_label)]
-    lcc = cc_size[np.argmax([cc[1] for cc in cc_size])]
-    lcc_idx = np.nonzero((cc_label==lcc[1]))[0]
-    
-    return g, lcc_idx
-    
-    
-def load_fibergraph(roi_fn, mat_fn):
-    
-    roix = roi.ROIXML(roi_fn+'.xml')
-    rois = roi.ROIData(roi_fn+'.raw', roix.getShape())
+    roix = roi.ROIXML(roiDir+bfn+'_roi.xml')
+    rois = roi.ROIData(roiDir+bfn+'_roi.raw', roix.getShape())
     
     fg = fibergraph.FiberGraph(roix.getShape(),rois,[])
-    fg.loadFromMatlab('fibergraph', '/mnt/braingraph1data/MRCAPgraphs/biggraphs/M87102217_fiber.mat')
+    fg.loadFromMatlab('fibergraph', graphDir+bfn+'_fiber.mat')
     
     return fg
-    
     
     
 
@@ -61,86 +80,91 @@ def get_roi(fg):
     
     return G,inroi,roival
 
-    
-def embed(G, d):
-    eA = Embed.Embed(d, Embed.self_matrix)
 
-    eA.embed(G)
-    return eA.get_scaled(d)
+def get_emb_from_fn(roiDir, graphDir, ccDir, embedDir, bfn,e):
+    print bfn
+    print "Loading LCC"
+    vcc = lcc.ConnectedComponent(fn=ccDir+bfn+'_concomp.npy')
+    print "Loading Graph"
+    fg = load_fibergraph(roiDir, graphDir, bfn)
+    print "Get Induced Subgraph, Binarize, Symetrize"
     
-def get_lcc_idx(G):
-    ncc,vertexCC = sp.cs_graph_components(G)
-        
-    cc_size = Counter(vertexCC)
-    cc_size = sorted(cc_size.iteritems(), key=lambda cc: cc[1],reverse=True)
-    cc_badLabel,_ = zip(*cc_size)
-    cc_dict = dict(zip(cc_badLabel, np.arange(ncc+1)))
-    
-    vertexCC = [cc_dict[vcc] for vcc in vertexCC]
-    
-    
-#    cc_size = [(cc,np.sum(cc_label==cc)) for cc in set(cc_label) if cc>0]
-#    lcc = cc_size[np.argmax([cc[1] for cc in cc_size])][0]
-#    lcc_idx = np.nonzero((cc_label==lcc))[0]
-#    
-    return np.array(vertexCC)
-    
-def save_lcc(fg, fn):
-    vcc = get_lcc_idx(fg.spcscmat)
-    
-    np.save(open(fn+'_concomp.npy','w'),vcc)
-    
-    savemat(fn+'_concomp.mat',{'vertexCC':vcc})
-    
-    return vcc
-    
-def cc_for_each_brain(fiberDir, roiDir, ccDir, figDir):
+    print "Embed"
+    X = e.embed(G).get_scaled()
 
-    fiberSfx = '_fiber.mat'
-    roiSfx = '_roi'
-    
-    brainFiles = [fn.split('_')[0] for fn in os.listdir(fiberDir)]
-    
-    for brainFn in brainFiles:
-        print "Processing brain "+brainFn
-        fg = load_fibergraph(roiDir+brainFn+roiSfx,fiberDir+brainFn+fiberSfx)
-                                   
-        vcc = save_lcc(fg, ccDir+brainFn)
-        
-        if figDir:
-            save_figures(get_cc_coords(vcc,10), figDir+brainFn)
-        
-        del fg
-        
-def get_cc_coords(vcc, ncc):
-    inlcc = (np.less_equal(vcc,ncc)*np.greater(vcc,0)).nonzero()[0]
-    coord = np.array([zindex.MortonXYZ(v) for v in inlcc])
+    if embedDir is not None:
+        np.save(embedDir+bfn+'_embed.npy',X)
+    return X
 
-    return np.concatenate((coord,vcc[inlcc][np.newaxis].T),axis=1)
-    
-def save_figures(coord, fn):
-    x,y,z,c = np.hsplit(coord,4)
-    
-    f = mlab.figure()
-    mlab.points3d(x,y,z,c, mask_points=50, scale_mode='none',scale_factor=2.0)
-    mlab.view(0,180)
-    mlab.savefig(fn+'_view0,180.png',figure=f,magnification=4)
-    mlab.view(0,90)
-    mlab.savefig(fn+'_view0,90.png',figure=f,magnification=4)
-    mlab.view(90,90)
-    mlab.savefig(fn+'_view90,90.png',figure=f,magnification=4)
-    mlab.close(f)
+
     
     
+def get_3d(x,shape):
+        """Takes a shape which is the shape of the new 3d image and 'colors' the image by connected component
+        
+        Input
+        =====
+        shape -- 3-tuple
+        
+        Output
+        ======
+        cc3d -- array of with shape=shape. colored so that ccz[x,y,z]=vcc[i] where x,y,z is the XYZ coordinates for Morton index i
+        """
+    
+        cc3d = np.NaN*np.zeros(shape)
+        allCoord = itt.product(*[xrange(sz) for sz in shape])
+        
+        [cc3d.itemset((xyz), x[zindex.XYZMorton(xyz)])
+            for xyz in allCoord if not x[zindex.XYZMorton(xyz)]==0];
+        return cc3d
+    
+def get_subgraph_density(A, v):
+        return sum(sum(A[:,v][v,:]))/(len(v)**2)
+
+def get_embedding_dim(G, dmax):
+    n = G.shape[0]
+    
+    m = int(np.sqrt(n))
+    
+    rhoBS = np.array([get_subgraph_density(G,np.random.random_integers(0,n-1, m)) for _ in xrange(1000)])
+    rhoHat = np.sort(.5-np.abs(.5-rhoBS))[950]
+    threshold = 2*np.sqrt(rhoHat*(1-rhoHat)*n)
+    
+    svec,sval,_ = sp.linalg.svds(G,dmax)
+    
+    return np.sum(sval>threshold)
+
+#def get_dim_for_scan(G,G2, v):
+#    neigh = G2[:,v].nonzero()[0]
+#    scan = G[:,neigh][neigh,:].toarray()
+#    n = scan.shape[0]
+#    return get_embedding_dim(scan,np.min((n-1,50)))
     
     
+def get_dim_for_scan(G, v):
+    neigh = G.neighbors(G.neighbors(v))
+    scan = G.Adj[:,neigh][neigh,:].toarray()
+    n = scan.shape[0]
+    return get_embedding_dim(scan,np.min((n-1,50)))
+
+
+
+def get_stfp_data_from_fn(roiDir, lccDir, embedDir, graphDir,bfn):
+    """Get the embedding, roi class labels, and graph corresponding to the largest connected component"""
+    fg = load_fibergraph(roiDir, graphDir, bfn)
+    vcc = lcc.ConnectedComponent(fn=lccDir+bfn+'_concomp.npy')
+
+    G = vcc.induced_subgraph(fg.spcscmat)
+    X = np.load(embedDir+bfn+'_embedding.npy')
     
-    
+    inccIdx = (vcc.vertexCC==1).nonzero()[0]
+    ccRoi = np.array([fg.rois.get(zindex.MortonXYZ(v)) for v in inccIdx])
+
+    return X, ccRoi, G
+
+
 if __name__=='__main__':
-    fiberDir = '/mnt/braingraph1data/MRCAPgraphs/biggraphs/'
-    roiDir = '/mnt/braingraph1data/MR.new/roi/'
-    ccDir = '/data/biggraphs/connectedcomp/'
-    figDir = '/home/dsussman/Dropbox/Figures/DTMRI/lccPics/'
+
 
     brainfiles = [fn.split('_')[0] for fn in os.listdir(fiberDir)]
     
